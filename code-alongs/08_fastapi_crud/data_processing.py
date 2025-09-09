1e294ee65ef09eeb2e39bacfc0185b19fe89a574
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-09-09 13:09:24 +0200

    crud exercise

---
+++ code-alongs/08_fastapi_crud/data_processing.py
@@ -0,0 +1,33 @@
+
+
+from constants import DATA_PATH
+import json
+from pprint import pprint
+from pydantic import BaseModel
+
+def read_json(filename: str):
+    with open(DATA_PATH / filename, "r") as file:
+        data = json.load(file)
+    return data
+
+
+class Book(BaseModel):
+    id: int 
+    title: str 
+    author: str 
+    year: int
+
+class Library(BaseModel):
+    name: str 
+    books: list[Book]
+
+def library_data(filename):
+    """Deserializes library json data into a Library model"""
+    json_data = read_json(filename)
+    return Library.model_validate(json_data)
+
+if __name__ == '__main__':
+
+    data = library_data("library.json")
+
+    pprint(data)
