8ba13d97201d627202763665d79dc8b725cae757
Jonas <jonas.gustafsson85@gmail.com>
2025-09-23 10:45:17 +0200

    fastapi ml model code along

---
+++ code-alongs/10_fastapi_ml_model/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from data_processing import IrisData, IrisInput, PredictionOutput
+import pandas as pd
+
+
+app = FastAPI()
+
+iris = IrisData()
+
+
+@app.get("/api/")
+def read_data():
+    return iris.to_json()
+
+@app.post("/api/predict", response_model = PredictionOutput)
+def predict_flower(payload: IrisInput):
+    data_to_predict = pd.DataFrame(payload.model_dump())
