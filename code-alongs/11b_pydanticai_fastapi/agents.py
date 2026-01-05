5621b1fa3a0b856e4ef2f4784b76bd6c9a1af39e
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2026-01-05 14:40:36 +0100

    stuff

---
+++ code-alongs/11b_pydanticai_fastapi/agents.py
@@ -0,0 +1,13 @@
+from pydantic_ai import Agent
+from dotenv import load_dotenv
+from data_models import Movie
+
+load_dotenv()
+
+movie_agents = Agent(
+    model="google-gla:gemini-2.5-flash",
+    system_prompt="""You are an expert in movies, based on a prompt,
+                you should find the closest mathcing movie
+                """,
+    output_type=Movie,
+)
---
+++ code-alongs/11b_pydanticai_fastapi/setup_db.py
@@ -0,0 +1,16 @@
+from utils import query_duckdb
+
+if __name__ == "__main__":
+
+    query_duckdb("""
+        CREATE TABLE IF NOT EXISTS movies(
+                title TEXT,
+                year INTEGER,
+                genre TEXT,
+                rating TINYINT
+                
+        );
+                
+    """)
+
+    print(query_duckdb("desc;"))
